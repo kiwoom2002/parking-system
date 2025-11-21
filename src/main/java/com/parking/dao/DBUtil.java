5c0294853e55e9a0aa5a0f0e37533ed3bc4e4cec
kiwoom2002 <kiwoom0802@gmail.com>
2025-11-21 22:22:45 +0900

    Update DBUtil.java

--- src/main/java/com/parking/dao/DBUtil.java
+++ src/main/java/com/parking/dao/DBUtil.java
@@ -9,7 +9,7 @@ public class DBUtil {
 	private static final String URL =
 	        "jdbc:mysql://localhost:3306/parking_db?serverTimezone=Asia/Seoul&useSSL=false&allowPublicKeyRetrieval=true";
     private static final String USER = "root";       // 내 MySQL 계정
-    private static final String PASSWORD = "0000";   // 내가 설정한 비밀번호
+    private static final String PASSWORD = "0000";   // 내 비밀번호
 
     static {
         try {
